595b2c172f2641fd24206d197793ea1b1f83f363
Raymond Toy <toy.raymond@gmail.com>
2013-12-14 11:58:44 -0800

    Add more tests for cos; new tests for tan.

--- ieee754/fdlibm-tests.lisp
+++ ieee754/fdlibm-tests.lisp
@@ -61,7 +61,7 @@
     (%cos (scale-float 1d0 -28))
   1d0)
 
-(rt:deftest cos.6
+(rt:deftest cos.4
     ;; Test for branch |x| < .3
     (%cos 0.25d0)
   0.9689124217106447d0)
@@ -81,9 +81,66 @@
     (%cos (/ pi 2))
   6.123233995736766d-17)
 
-(rt:deftest cos.8
+(rt:deftest cos.misc.1
     ;; Test for argument reduction
     (%cos (scale-float 1d0 120))
   -0.9258790228548379d0)
 
+(rt:deftest cos.red.0
+    ;; Test for argument reduction with n mod 4 = 0
+    (%cos (* 7/4 pi))
+  7.07106781186547372858534520893509069186435867941d-1)
 
+(rt:deftest cos.red.1
+    ;; Test for argument reduction with n mod 4 = 1
+    (%cos (* 9/4 pi))
+  7.0710678118654771924095701509080985020443197242d-1)
+
+(rt:deftest cos.red.2
+    ;; Test for argument reduction with n mod 4 = 2
+    (%cos (* 11/4 pi))
+  -7.07106781186546658225945423833643190916000739026d-1)
+
+(rt:deftest cos.red.3
+    ;; Test for argument reduction with n mod 4 = 3
+    (%cos (* 13/4 pi))
+  -7.07106781186547177799579165130055836531929091466d-1)
+
+(rt:deftest tan.1
+    (%tan 0d0)
+  0d0)
+
+(rt:deftest tan.2
+    (%tan -0d0)
+  -0d0)
+
+(rt:deftest tan.3
+  ;; |x| < 2^-28
+    (%tan (scale-float 1d0 -29))
+  #.(scale-float 1d0 -29))
+
+(rt:deftest tan.4
+    ;; |x| < .6744
+    (%tan 0.5d0)
+  5.4630248984379051325517946578028538329755172018d-1)
+
+(rt:deftest tan.4
+    ;; |x = 11/16 = 0.6875 > .6744
+    (%tan (float 11/16 1d0))
+  8.21141801589894121911423965374711700875371645309d-1)
+
+(rt:deftest tan.red.0
+    ;; Test for argument reduction with n even
+    (%tan (* 7/4 pi))
+  -1.00000000000000042862637970157370388940976433505d0)
+
+(rt:deftest tan.red.1
+    ;; Test for argument reduction with n odd
+    (%tan (* 9/4 pi))
+  9.99999999999999448908940383691222098948324989275d-1)
+
+(rt:deftest tan.misc.1
+    (%tan (scale-float 1d0 120))
+  -4.08066388841804238545143494525595117765084022768d-1)
+  
+  
